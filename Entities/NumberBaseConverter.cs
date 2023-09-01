6562dd3adbb6265e8784b81115fa957c86522d55
oskrcode <oskr.rodriguezbastidas@gmail.com>
2023-09-01 16:27:30 -0500

    Add new class of conversion

---
+++ Entities/NumberBaseConverter.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalSytemWithCSharp.Entities
+{
+    public class NumberBaseConverter
+    {
+        private string digits = "0123456789ABCDEFG";
+        public double Number { get; set; }
+
+        public int BaseNumber { get; set; }
+
+        private int integerPart = 0;
+
+        private double decimalPart = 0.0;
+        public NumberBaseConverter(double number, int baseNumber= 10)
+        {
+            (Number,BaseNumber) = (number, BaseNumber);
+            integerPart = (int)number;
+            decimalPart = number - integerPart;
+        }
+
+        private StringBuilder ConvertDecimalToOther(int baseConvert)
+        {
+            StringBuilder binaryNumber = new StringBuilder();
+            do
+            {
+                binaryNumber.Insert(0, digits[integerPart % baseConvert]);
+                integerPart /= baseConvert;
+            } while (integerPart > 0);
+
+            if (decimalPart > 0)
+            {
+                binaryNumber.Append(".");
+
+                do
+                {
+                    decimalPart *= baseConvert;
+                    binaryNumber.Append(digits[(int)decimalPart]);
+                    decimalPart -= (int)decimalPart;
+
+                } while (decimalPart > 0);
+
+            }
+
+
+            return binaryNumber;
+
+
+        }
+
+        public StringBuilder getNumberBaseConvert(int baseConvert)
+        {
+            return ConvertDecimalToOther(baseConvert);
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using DigitalSytemWithCSharp.Entities;
-var number = new ConvertNumericalBase(255);
-number.ConvertDecimalToOther(16);
+var number = new ConvertNumericalBase(255.55);
+number.ConvertDecimalToOther(2);
+
+var n = new NumberBaseConverter(255.55);
+Console.WriteLine(n.getNumberBaseConvert(2));
 
 
